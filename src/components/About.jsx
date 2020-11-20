ef1aa202a5fdde2042ee43728b4c8397a9bbefb1
cristianluca19 <cristianlucatti@gmail.com>
2020-11-20 14:58:00 -0300

    Deploy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,8 @@ export default function About() {
 
           <p>Para mas info</p>
           
-            <a href='https://www.linkedin.com/in/cristian-lucatti/'><img src="https://img.icons8.com/cute-clipart/128/000000/linkedin.png"/></a>
-            <a href='https://github.com/cristianluca19'><img src="https://img.icons8.com/cute-clipart/128/000000/github.png"/></a>
+            <a href='https://www.linkedin.com/in/cristian-lucatti/'><img src='https://img.icons8.com/cute-clipart/128/000000/linkedin.png'/></a>
+            <a href='https://github.com/cristianluca19'><img src='https://img.icons8.com/cute-clipart/128/000000/github.png'/></a>
     
         </div>
 
